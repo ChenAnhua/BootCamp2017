50928f1c32b7952ffeac5b447cd95a0c96b68efb
Anhua CHEN <anhua@uchicago.edu>
2017-06-22 14:34:46 -0500

    add unit testing

---
+++ probsets/computation/probset1/test_solutions.py
@@ -0,0 +1,46 @@
+# test_solutions.py
+"""Volume 1B: Testing.
+<Name>
+<Class>
+<Date>
+"""
+
+import solutions as soln
+import pytest
+
+# Problem 1: Test the addition and fibonacci functions from solutions.py
+def test_addition():
+    assert soln.addition(3, 4) == 7, "Addition failed on positive integers"
+
+def test_smallest_factor():
+    assert soln.smallest_factor(1) == 1, "Smallest factor failed on 1"
+    assert soln.smallest_factor(2) == 2, "smallest factor failed on primes"
+    assert soln.smallest_factor(4) == 2, "smallest factor failed on composites"
+
+# Problem 2: Test the operator function from solutions.py
+def test_operator():
+    pass
+
+# Problem 3: Finish testing the complex number class
+@pytest.fixture
+def set_up_complex_nums():
+    number_1 = soln.ComplexNumber(1, 2)
+    number_2 = soln.ComplexNumber(5, 5)
+    number_3 = soln.ComplexNumber(2, 9)
+    return number_1, number_2, number_3
+
+def test_complex_addition(set_up_complex_nums):
+    number_1, number_2, number_3 = set_up_complex_nums
+    assert number_1 + number_2 == soln.ComplexNumber(6, 7)
+    assert number_1 + number_3 == soln.ComplexNumber(3, 11)
+    assert number_2 + number_3 == soln.ComplexNumber(7, 14)
+    assert number_3 + number_3 == soln.ComplexNumber(4, 18)
+
+def test_complex_multiplication(set_up_complex_nums):
+    number_1, number_2, number_3 = set_up_complex_nums
+    assert number_1 * number_2 == soln.ComplexNumber(-5, 15)
+    assert number_1 * number_3 == soln.ComplexNumber(-16, 13)
+    assert number_2 * number_3 == soln.ComplexNumber(-35, 55)
+    assert number_3 * number_3 == soln.ComplexNumber(-77, 36)
+
+# Problem 4: Write test cases for the Set game.
---
+++ probsets/computation/probset1/unitTestingProbset.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 13:41:45 2017
+
+@author: Chen Anhua 
+
+This scrip is for unit testing problem set
+
+
+"""
+
+
+
+
+
